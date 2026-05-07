645250035a179acabf60a749a6cc95d573db73d4
Gio2025 <george.athanasopoulos46@student.qcc.cuny.edu>
2026-05-07 17:22:22 +0000

    Started Project

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -1,5 +1,20 @@
 import sqlite3
 import os
-
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
+app = Flask(__name__)
+app.secret_key = "dev_secret_key"
+
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def get_db():
+    conn = sqlite3.connect("student_app.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+    
